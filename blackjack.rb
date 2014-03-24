2a312e3fecada12f74b8ae61566e60cebc10b4ce
stephenkaraga <stephenkaraga@gmail.com>
2014-03-24 16:51:52 -0400

    Fixed syntax error

--- blackjack.rb
+++ blackjack.rb
@@ -5,9 +5,6 @@ cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K',         'A
 
 deck = cards.product(suits)
 
-
-
-
 #deck.shuffle # the deck is now shuffled
 
 
@@ -16,17 +13,14 @@ deck = cards.product(suits)
 #		  'AH'=>11, '2H'=> 2, '3H'=>3, '4H'=>4, '5H'=>5, '6H'=>6, '7H'=>7, '8H'=>8, '9H'=>9, '10H'=>10, 'JH'=>10, 'KH'=>10, 'QH'=>10,
 #         'AS'=>11, '2S'=> 2, '3S'=>3, '4S'=>4, '5S'=>5, '6S'=>6, '7S'=>7, '8S'=>8, '9S'=>9, '10S'=>10, 'JS'=>10, 'KS'=>10, 'QS'=>10}
 #
-
-
 #dealer_hand = [deckForGame1(0), deckForGame1(2)]
 #
-player_hand = [deckForGame1(1), deckForGame1(3)]
-
+#player_hand = [deckForGame1(1), deckForGame1(3)]
 
 # Stephen sharing
 
-puts "Hello Player1! Welcome to BlackJack World! Type 'yes' when you are ready to play"
-input = gets.chomp
+puts "Hello! Welcome to BlackJack World! What is your name?"
+name = gets.chomp
 deckforGame1 = deck.shuffle
 player1Hand = []
 computerHand = []
@@ -42,52 +36,48 @@ computers_hand_value = 0
 player1Hand.push(card1, card3)
 computerHand.push(card2, card4)
 
-def checkAce(cardsArray)
-  flag = false
+#This method takes a card array and calculates the numeric value of the hand 
+#and returns the value.
+def calculate_valueof_hand(cardsArray)
+  aceFlag = false
   aceCount = 0
   cardsArray.each do |card|
     if card.first == 'A'
       flag = true
       aceCount +=1
     end
-end
-
-#This method takes a card array and calculates the numeric value of the hand and returns the value
-def calculate_valueof_hand(cardsArray)
-  hand_value = 0
-  cardsArray.each do |card|
+  end
+    hand_value = 0
+   if !aceFlag
+    cardsArray.each do |card|
     cardValue = card.first.to_i
     if cardValue != 0
       hand_Value += cardValue
-    elsif cardValue == 0 && card.first != "A"
+      else cardValue == 0 && card.first != "A"
         hand_Value += 10
-      else
-        hand_Value += 11
       end
-  end
-  doestheHandIncludeAceFlag= false
-  if hand_Value > 21 && #does the hand include an ace
-    doestheHandIncludeAceFlag = true
-    #check for ace and recalculate hand_value
-  end
+    end
+end
   hand_value
 end
 
 players_hand_value = calculate_valueof_hand(player1Hand)
 computers_hand_value = calculate_valueof_hand(computerHand)
 
-puts "Hello Player 1. Your hand includes: " + player1Hand.first.first + "of" + player1Hand.first.last +
-  "the " + player1Hand[1].first " of " + player1Hand[1].last + ". Computer is showing
-" + computerHand.first.first + " of " + computerHand.first.last + ". Would
-you like to hit or stay? Please enter 'hit' or 'stand'."
+puts "Hello Player 1. Your hand includes: the " 
++ player1Hand[0].first + " of " + player1Hand[0].last +
+  " and the " + player1Hand[1].first + " of " + player1Hand[1].last + ". Computer is showing the " + computerHand[0].first + " of " + computerHand.first.last + ". Would
+you like to hit or stand? Please enter 'hit' or 'stand'."
 input = gets.chomp
-
+while input.downcase != 'hit' && input!= 'stand'
 if input.downcase == 'hit'
   card5 = deckforGame1.shift
   player1Hand.push(card5)
   players_hand_value = calculate_valueof_hand(player1Hand)
 elsif input.downcase == 'stand'
+  #Computer finishes the hand and display the outcome to user.
 else
   puts "Please enter hit or stand"
   input = gets.chomp
+  end
 end
